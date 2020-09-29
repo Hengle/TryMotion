7f26005e629ec0cccb5af145d39053c4c3e9b9ec
huagexin777 <huagexin777@163.com>
2020-09-29 20:03:36 +0800

    lin
    添加 资源

---
+++ TryMotion/Assets/FpsGame/Scripts/CharacterAiming.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace fpsGame
+{
+    public class CharacterAiming : MonoBehaviour
+    {
+        Camera mainCam;
+        [SerializeField] float turnSpeed = 15;
+
+
+        void Start()
+        {
+            mainCam = Camera.main;
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        void FixedUpdate()
+        {
+            //float yawCam = mainCam.transform.rotation.y;
+            //transform.rotation = Quaternion.Slerp(transform.rotation , Quaternion.Euler(new Vector3(0 , yawCam , 0)),Time.fixedDeltaTime * turnSpeed);
+        }
+    }
+
+}
